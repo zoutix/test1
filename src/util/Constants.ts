ff1d6bb15c6eef144212b0a1d4634c760e0374f4
zoutix <zoutix2005@gmail.com>
2026-07-19 03:31:55 +0300

    Add shared constants for voxel project

---
+++ src/util/Constants.ts
@@ -0,0 +1,8 @@
+export const CHUNK_SIZE = 16;
+export const RENDER_DISTANCE = 6;
+export const PLAYER_HEIGHT = 1.8;
+export const PLAYER_RADIUS = 0.3;
+export const GRAVITY = 20;
+export const WALK_SPEED = 5;
+export const SPRINT_SPEED = 8;
+export const JUMP_VELOCITY = 8.5;
